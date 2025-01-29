a5176b8e6b82fe197fd681aeea41a342a3d1318f
Jonathan Eichenberger <djohnni@hotmail.com>
2025-01-29 08:06:47 -0300

    Estilizado menu-bar e criados os components do card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardLabelComponent } from './card-label/card-label.component';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [ ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
