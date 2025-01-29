60c8f18366ccd3164ac85dcaac954628318e2dc0
Jonathan Eichenberger <djohnni@hotmail.com>
2025-01-29 08:31:11 -0300

    Estilizando o car-label component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardLabelComponent } from './card-label/card-label.component';
+import { CardPricingComponent } from './card-pricing/card-pricing.component';
 
 @Component({
   selector: 'app-card',
-  imports: [ ],
+  imports: [ CardLabelComponent, CardPricingComponent ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
